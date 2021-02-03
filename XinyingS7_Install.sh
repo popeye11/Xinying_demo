8f8523883849e568314736f9de8fdf7ac6cd4d27
Xiaodong Zhang <Xiaodong.Zhang@toptica.com>
2021-02-03 11:33:59 +0100

    remove wget ngrok from .sh

--- XinyingS7_Install.sh
+++ XinyingS7_Install.sh
@@ -9,7 +9,7 @@ cd snap7-debian/build/unix && sudo make -f arm_v7_linux.mk all
 sudo cp ../bin/arm_v7-linux/libsnap7.so /usr/lib/libsnap7.so
 sudo cp ../bin/arm_v7-linux/libsnap7.so /usr/local/lib/libsnap7.so
 sudo ldconfig
-sudo wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+#sudo wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
 #sudo unzip ngrok-stable-linux-arm.zip
 #sudo ./ngrok authtoken 1nsuGo6Ddbbgi4AZZp17rXEOUrK_6Jb5Rkiwy4YU6H73x7h4g
 sudo pip3 install python-snap7
